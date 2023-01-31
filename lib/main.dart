8601c98a713917f1b16cc98d25c4becbc6a8a06a
DongHyun <danny001006@naver.com>
2023-01-31 11:56:34 +0900

    init settings

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:cal_sch/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(
+      fontFamily: 'NotoSans',
+    ),
+    home: HomeScreen(),
+   ),
+  );
+}
+
